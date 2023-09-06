79550d0eea887b5ab8b8ffe96ea360a2bc89bba0
Auriane Chay <chaya.07@protonmail.com>
2023-09-06 17:40:46 +0200

    feat: sky texture as background

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const texture = new THREE.TextureLoader().load( "images/sky.jpg" );
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+scene.background = texture
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
